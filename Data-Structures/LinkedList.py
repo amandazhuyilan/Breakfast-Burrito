ad9fbf8bbcf1121c21ca7a28ec5cf73fb9c783cc
Amanda Zhu <amandazhu@Amandas-MacBook-Pro.local>
2017-10-09 15:19:19 -0400

    Linked list and its operations.

--- Data-Structures/LinkedList.py
+++ Data-Structures/LinkedList.py
@@ -1,41 +1,82 @@
-# Single linked-list
-class Single_Linked_List_Node(object):
+# Single linked-list contructor
+class Node(object):
 	def __init__(self, data = None, next_node = None):
 		self.data = data
-		self.next_node = next_node
+		self.next = next
 
 	def get_data(self):
 		return self.data
 
 	def get_next(self):
-		return self.next_node
+		return self.next
 
 	def set_next(self, new_next):
-		self.next_node = new_next 
+		self.next = new_next 
 
+# Single linked list operations
 class Single_Linked_List(object):
-	def __init__(object):
-		self.head = head
+	def __init__(self):
+		self.head = None
+
+	def isEmpty(self):
+		return self.head == None
 
 	def insert(self, data):
-		new_node = Single_Linked_List_Node(data)
+		new_node = Node(data)
 		new_node.set_next(self.head)
 		self.head = new_node
 
 	def size(self):
-		cuurent 
+		current = self.head 
+		count = 0 
+		while current:
+			count += 1
+			current = current.get_next()
+		return count
 
-class Double_Linked_List_Node(object):
-	def __init__(self, data = None, next_node = None, previous_node = None):
-		self.data = data
-		self.next_node = next_node 
-		self.previous node = previous_node 
+	def search(self, data):
+		current = self.head
+		found = False
+		while current != None and not found:
+			if current.get_data() == data:
+				found = True 
+			else:
+				current = current.get_next()
+		return found 
 
-	def get_data(self):
-		return self.data = data
+	def  delete(self, data):
+		if self.head is None:
+			return False
+		else:
+			current = self.head
+			previous = None
+			found = False
 
-	def get_next(self):
-		return self.next_node 
+			while not found:
+				if current.get_data() == data:
+					found = True 
+				else:
+					previous = current 
+					current = current.get_next()
 
-	def set_next(self, new_next):
-		self.next_node = new_next
+			if previous == None:
+				self.head = current.get_next()
+			else:
+				previous.set_next(current.get_next())
+
+# TEST_CASE:
+mylist = Single_Linked_List()
+
+mylist.insert(31)
+mylist.insert(77)
+mylist.insert(87)
+mylist.insert(3)
+mylist.insert(24)
+
+print("Original size: " + str(mylist.size()))
+print("Search for 77: " + str(mylist.search(77)))
+
+mylist.delete(24)
+
+print("Size after removing one element: " + str(mylist.size()))
+
