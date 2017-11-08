082a39ff8c505febf7efebb0fe57dc951aae5fe1
Amanda Zhu <amandazhu@Amandas-MacBook-Pro.local>
2017-11-08 14:30:49 -0500

    switch 'O' surrounded by 'X' into 'X'

---
+++ Problems-and-Solutions/SurroundedRegions.py
@@ -0,0 +1,34 @@
+def surroundedRegions(self, board):
+    # write your code here
+    for i in range(len(board)):             #rows
+        for j in range(len(board[0])):      #columns
+            if board[0][j] == 'O':
+                board[0][j] = 'D'
+
+            if board[i][0] == 'O':
+                board[i][0] = 'D'
+                
+            if board[i][len(board[0])-1] == 'O':
+                board[i][len(board[0])-1] = 'D'
+                
+            if board[len(board)-1][j] == 'O':
+                board[len(board)-1][j] = 'D'
+                
+    for i in range(1,len(board)-1):             #rows
+        for j in range(1,len(board[0])-1):      #column
+            if board[i][j] == 'O':
+                if board[i+1][j] == 'D' or board[i][j+1] == 'D' or board[i-1][j] == 'D' or board[i][j-1] == 'D':
+                    board[i][j] = 'D'
+
+    for i in range(len(board)):             #rows
+        for j in range(len(board[0])):      #columns
+            if board[i][j] == 'O':
+                board[i][j] = 'X'
+
+            if board[i][j] == 'D':
+                board[i][j] = 'O'
+
+                
+                    
+            
+           
