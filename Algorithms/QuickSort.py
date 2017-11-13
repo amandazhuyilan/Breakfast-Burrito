b069ebc663240c9312283d67c283ef8ef77cee4b
Amanda Zhu <amandazhu@Amandas-MacBook-Pro.local>
2017-11-13 17:08:29 -0500

    Added new in-place quicksort method

--- Algorithms/QuickSort.py
+++ Algorithms/QuickSort.py
@@ -4,31 +4,55 @@
 # more array, else if less than put in less array, put element in equal 
 # array if it equals to pivot. 
 # Do this recursively and finally return the sorted array.
-def QuickSort(In_Arr):
+
+def QuickSort(A):
+	if len(A) < 2:
+		return A
+
+	equal = []
 	less = []
 	more = []
-	equal = []
 
-	if len(In_Arr) > 1:
-		# Pick last element of array as pivot
-		pivot = In_Arr[-1]
-		for i in range(len(In_Arr)):
-			if pivot > In_Arr[i]:
-				less.append(In_Arr[i])
-			elif pivot < In_Arr[i]:
-				more.append(In_Arr[i])
-			elif pivot == In_Arr[i]:
-				equal.append(In_Arr[i])
+	pivot = A[-1]
 
-		return QuickSort(less) + equal + QuickSort(more)
+	for i in A:
+		if i < pivot:
+			less.append(i)
+		elif i > pivot:
+			more.append(i)
+		elif i == pivot:
+			equal.append(i)
 
-	else:
-		return In_Arr
+	return QuickSort(less) + equal + QuickSort(more)
 
+def QuickSort_in_place(A, start, end):
+	if start >= end:
+		return 
+
+	left = start
+	right = end 
+	pivot = A[(start+end)//2]
+
+	while left <= right:
+		while left <= right and A[left] < pivot:    # if the elements are in their right places, leave them alone and move the left/right pointers to the next one
+			left += 1
+
+		while left <= right and A[right] > pivot:
+			right -= 1
+
+		if left <= right:
+			A[left], A[right] = A[right], A[left]
+			left += 1
+			right -= 1
+
+	QuickSort_in_place(A, start, right)
+	QuickSort_in_place(A, left, end)
+
+	return A
 
 TEST_CASE = [10,5,45,3,2,12]
 print(QuickSort(TEST_CASE))
-
+print(QuickSort_in_place(TEST_CASE,0,5))
 
 
 
