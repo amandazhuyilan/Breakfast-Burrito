bbd08177405cd607190ef0aafac6fc4b85da4beb
Yilan Zhu <zhu.yil@husky.neu.edu>
2018-01-17 15:26:41 -0500

    updated climbing stairs with DP

--- Problems-and-Solutions/climbingStairs.py
+++ Problems-and-Solutions/climbingStairs.py
@@ -7,9 +7,14 @@ class Solution:
 		return self.fib(numStairs + 1)
 
 	def fib(self, n):
-		if n <= 1:
-			return n
-		else: return self.fib(n-1) + self.fib(n-2)
+		fib = []
+		fib.insert(0, 0)
+		fib.insert(1, 1)
+
+		for i in range(2, n + 1):
+			fib.insert(i, fib[i - 1] + fib[i - 2])
+
+		return fib[n]
 
 	def climbStairsMultiple(self, numStairs, numSteps):
 		return self.fib_multiple(numStairs + 1, numSteps)
@@ -22,3 +27,5 @@ class Solution:
 			result = result + self.fib_multiple(n-1, m) + self.fib_multiple(n-2, m)
 			return result
 
+print(Solution().climbStairsMultiple(4,2))
+print
