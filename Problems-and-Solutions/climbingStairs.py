d67c95d1e0beb512e0e4f6e0977dd8750cf25fdc
Yilan Zhu <zhu.yil@husky.neu.edu>
2018-01-17 13:53:39 -0500

    added climbing stairs question with recursion

--- Problems-and-Solutions/climbingStairs.py
+++ Problems-and-Solutions/climbingStairs.py
@@ -1,3 +1,7 @@
+# Caluculate the different ways of climbing the stairs, assuming this person can only climb 1 or 2 steps at a time
+
+# Solved by using recursion
+
 class Solution:
 	def climbStairs(self, numStairs):
 		return self.fib(numStairs + 1)
@@ -7,4 +11,14 @@ class Solution:
 			return n
 		else: return self.fib(n-1) + self.fib(n-2)
 
-print(Solution().climbStairs(5))
+	def climbStairsMultiple(self, numStairs, numSteps):
+		return self.fib_multiple(numStairs + 1, numSteps)
+
+	def fib_multiple(self, n, m):
+		result = 0
+		for i in range(m):
+			if n <= 1:
+				return n
+			result = result + self.fib_multiple(n-1, m) + self.fib_multiple(n-2, m)
+			return result
+
