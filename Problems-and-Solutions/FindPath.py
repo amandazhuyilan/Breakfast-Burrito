4d32364fa8189158a06229beac52ae531036ff0e
Yilan Zhu <zhu.yil@husky.neu.edu>
2018-02-21 23:08:53 -0500

    added some G problems and path problems

---
+++ Problems-and-Solutions/FindPath.py
@@ -0,0 +1,56 @@
+def find_path(graph, start, end, path = []):
+	path = path + [start]
+	if start == end:
+		return path
+	if start not in graph:
+		return None
+	for node in graph[start]:
+		if node not in path:
+			newpath = find_path(graph, node, end, path)
+			if newpath:
+				return newpath
+	return None
+
+
+def find_all_paths(graph, start, end, path = []):
+	path = path + [start]
+	if start == end:
+		return [path]
+	if start not in graph:
+		return None
+
+	paths = []
+	for node in graph[start]:
+		if node not in path:
+			newpaths = find_all_paths(graph, node, end, path)
+			for newpath in newpaths:
+				paths.append(newpath)
+	return paths
+
+def find_shortest_path(graph, start, end, path = []):
+	path = path + [start]
+	if start == end:
+		return path
+	if start not in graph:
+		return None	
+	shortest = None
+	for node in graph[start]:
+		if node not in path:
+			 newpath = find_shortest_path(graph, node, end, path)
+			 if newpath:
+			 	if not shortest or len(newpath) < len(shortest):
+			 		shortest = newpath
+
+	return shortest
+graph = {
+	'A' : ['B', 'C'],
+	'B' : ['A','C','D'],
+	'C' : ['A','B','D','E'],
+	'D' : ['B','C'],
+	'E' : ['C','F'],
+	'F' : ['E']
+}
+
+print("Find a path: ", find_path(graph, 'A', 'F',[]))
+print("Find all paths: ", find_all_paths(graph, 'A','F',[]))
+print("Find shortest path: ", find_shortest_path(graph, 'A','D',[]))
---
+++ Problems-and-Solutions/G_pacman.py
@@ -0,0 +1,5 @@
+# pac-man game简化版，给一个board，一个start，一个destination，某些点上面有ghost，ghost不会动，pac-man每次可以上下左右动一格，问能不能从start走到destination。
+# dfs / bfs 都行，问了问两种方法的区别，让随便挑一个写码。
+# 写了个dfs的，跑了跑 test case。. 鐗涗汉浜戦泦,涓€浜╀笁鍒嗗湴
+# follow up:
+# 如果ghost每次也会动一格，问能不能保证pac-man能到达终点。只说思路，不用写码。S
