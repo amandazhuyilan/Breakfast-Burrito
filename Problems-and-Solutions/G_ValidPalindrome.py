5bca1e648a168c9e099dd305abcec30decd6c37d
Yilan Zhu <zhu.yil@husky.neu.edu>
2018-03-12 22:57:38 -0400

    preparing for G interview

--- Problems-and-Solutions/G_ValidPalindrome.py
+++ Problems-and-Solutions/G_ValidPalindrome.py
@@ -1,45 +1,36 @@
-# Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if s is None:
+            return False
+        temp = []
+        s = list(s)
+        # if '(' not in s and'{' not in s  and '[' not in s:
+        #     return False
+        
+        for i in s:
+            if i == '(' or i == '[' or i == '{':
+                temp.append(i)
+                # continue
+            if i == ')':
+                if len(temp) > 0 and temp[-1] == '(':
+                    temp = temp[:-1]
+                else:
+                    return False
+            if i == ']':
+                if len(temp) > 0 and temp[-1] == '[':
+                    temp = temp[:-1]
+                else:
+                    return False
+            if i == '}':
+                if len(temp) > 0 and temp[-1] == '{':
+                    temp = temp[:-1]
+                else:
+                    return False
 
-# Example 1:
-# Input: "aba"
-# Output: True
-# Example 2:
-# Input: "abca"
-# Output: True
-# Explanation: You could delete the character 'c'.
-
-def validPalindrome(input_string):
-	if input_string is None:
-		return None
-
-	string_list = []
-	for i in input_string:
-		string_list.append(i)
-
-	for i in range(len(string_list)):
-		element = string_list[i]
-		del string_list[i]
-
-		if isPanlindrome(string_list) is True:
-			return True
-		else:
-			string_list.insert(i,element)
-
-	if isPanlindrome(string_list) is False:
-		return False
-
-def isPanlindrome(input_string):
-	if input_string is None:
-		return None
-	start = 0 
-	end = len(input_string) - 1
-
-	while start < end:
-		if input_string[start] != input_string[end]:
-			return False
-		else:
-			start += 1
-			end -= 1
-	return True 
-
-print(validPalindrome("abbga"))
+        if len(temp) == 0:
+            return True
+        return False
