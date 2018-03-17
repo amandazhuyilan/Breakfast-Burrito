539f3d164acc4f5e5a29a59d9702227e92511afe
Yilan Zhu <zhu.yil@husky.neu.edu>
2018-03-17 17:23:54 -0400

    permutation

--- Problems-and-Solutions/GenParentheses.py
+++ Problems-and-Solutions/GenParentheses.py
@@ -13,7 +13,6 @@ class Solution:
 
         for k in result_int:
             result_temp = []
-            print(k)
             for j in k:
                 result_temp.append(self.genPairs(j))
             result.append(''.join(result_temp))
@@ -27,15 +26,38 @@ class Solution:
             result.append(')')
         return ''.join(result)
 
+    # def partition(self, n):
+    #     if n == 0:
+    #         yield []
+    #         return
+
+    #     for p in self.partition(n-1):
+    #         print(p)
+    #         yield [1] + p
+    #         if p and (len(p) < 2 or p[1] > p[0]):
+    #             yield [p[0] + 1] + p[1:]
+
     def partition(self, n):
         if n == 0:
             yield []
             return
 
-        for p in self.partition(n-1):
-            yield [1] + p
-            if p and (len(p) < 2 or p[1] > p[0]):
-                yield [p[0] + 1] + p[1:]
+        for p in self.partition(n - 1):
+            print("p1: ",p)
+            p.append(1)
+            print("p2: " ,p)
+            yield p
+            p.pop()
+            print("p3: ", p)
+            if len(p) == 1 or (len(p) > 1 and p[-1] < p[-2]):
+                p[-1] += 1
+                yield p
+                p[-1] -= 1
+
+
 
 s = Solution()
-print(s.generateParenthesis(4))
+# print(s.generateParenthesis(4))
+
+for p in s.partition(5):
+    print(p)
--- Problems-and-Solutions/permutations.py
+++ Problems-and-Solutions/permutations.py
@@ -1,22 +1,38 @@
 
 class Solution:
-    def permutation(self,nums):
+    def permutation(self, nums):
         result = []
         self.permute(nums, 0, result)
         return result
 
-    def permute(self,nums, startIndex,result):
-        if startIndex==len(nums):
+    def permute(self, nums, startIndex, result):
+        if startIndex == len(nums):
             result.append([] + nums)
         else:
-            for i in range(startIndex,len(nums)):
+            for i in range(startIndex, len(nums)):
                 nums[startIndex], nums[i] = nums[i], nums[startIndex]
                 self.permute(nums, startIndex + 1, result)
-                nums[startIndex], nums[i] = nums[i], nums[startIndex] # backtrack
+                nums[startIndex], nums[i] = nums[i], nums[startIndex]  # backtrack
 
-     
+
+    def permutation_string(self, s):
+        s = list(s)
+
+        result = []
+        self.permutation_string_recur(s, 0, result)
+        return result
+
+    def permutation_string_recur(self, s, startIndex, result):
+        if startIndex == len(s):
+            print([] + s)
+            result.append([] + s)
+        else:
+            for i in range(startIndex, len(s)):
+                s[i], s[startIndex] = s[startIndex], s[i]
+                self.permutation_string_recur(s, startIndex + 1, result)
+                s[i], s[startIndex] = s[startIndex], s[i]
 # # Driver program to test the above function
 nums = [1,2,3]
+s = "abc"
 print(Solution.permutation(Solution(),nums))
-#print(permutation([1,2,3]))
-
+print(Solution.permutation_string(Solution(), s))
